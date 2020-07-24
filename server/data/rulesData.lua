09b8f363765a5f130cfcceb80ef7d2cabbd4286d
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-24 17:35:21 -0400

    Create rulesData.lua

---
+++ server/data/rulesData.lua
@@ -0,0 +1,11 @@
+local rulesData ={}
+
+export type rules = 
+    {
+        mode: string;
+        minimumPlayers: number;
+        maximumPlayers: number;
+        modeType: string;
+    }
+
+    return rulesData 
