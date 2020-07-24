024ad3c9a4e0219dcd11e7c2607291660f5bcb52
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-24 17:35:18 -0400

    Update playlistClass.lua

--- server/sys/playlistSystem/playlistClass.lua
+++ server/sys/playlistSystem/playlistClass.lua
@@ -1,8 +1,19 @@
 local playlistClass = {}
 playlistClass.__index = playlistClass 
 
-function playlistClass.new(rules: playlistRules)
-    
+local rulesData = _G.get "data/rulesData"
+
+local rules = rulesData.rules
+function playlistClass.new(playlistRules)
+    local self = setmetatable(playlistRules, playlistClass)
+    print(self.mode)
+    self.playersInQueue = {}
+    return self
 end 
+
+function playlistClass:PlayerAddedToQueue(playerObject)
+
+end 
+
 
 return playlistClass 
