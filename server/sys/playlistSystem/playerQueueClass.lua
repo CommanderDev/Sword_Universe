51c0389c3901b0ad8c51f4b563ed18ea46574b4a
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-25 05:12:56 -0400

    Update playerQueueClass.lua

--- server/sys/playlistSystem/playerQueueClass.lua
+++ server/sys/playlistSystem/playerQueueClass.lua
@@ -11,12 +11,13 @@ local DataStore2 = _G.get "DataStore2"
 local playerQueueClass = {}
 playerQueueClass.__index = playerQueueClass 
 
-function playerQueueClass.new(playerObject, playlistClass)
+function playerQueueClass.new(playerObject, skillRating,playlistClass)
     local self = setmetatable({}, playerQueueClass)
     coroutine.wrap(function()
         self.playerObject = playerObject
         self.playlistClass = playlistClass
         self.skillgapRadius = matchmakingData["minimumSkillGap"]
+        self.skillRating = skillRating
         self.eligiblePlayers = {self} --The players eligible to queue
         self.foundMatch = false
     end)()
@@ -29,19 +30,25 @@ function playerQueueClass:SearchForPlayers()
     local playersInMatch = {} --Only used if a match is found. Makes it so all the players are added to the match array accordingly.
     for index, playerClass in next, self.playlistClass.playersInQueue do
         if(typeof(playerClass) == "table" and playerClass.playerObject) then 
+            print(playerClass.skillRating)
             if(self.playerObject ~= playerClass.playerObject and playerClass.skillRating) then 
-                if(self.skillRating-playerClass.skillRating <= self.skillgapRadius and 0-self.skillRating+playerClass.skillRating <= 0-self.skillgapRadius) then 
+                print(index)
+                print(self.skillRating+playerClass.skillRating)
+                if(self.skillgapRadius < self.skillRating - playerClass.skillRating and self.skillgapRadius < self.skillRating+playerClass.skillRating) then return end
+               -- if(self.skillRating-playerClass.skillRating > self.skillgapRadius or 0-self.skillRating+playerClass.skillRating > 0-self.skillgapRadius) then return end 
+                --if(self.skillRating-playerClass.skillRating <= self.skillgapRadius and 0-self.skillRating+playerClass.skillRating <= 0-self.skillgapRadius) then 
                     print("Amount required reached!")
                     table.insert(self.eligiblePlayers, playerClass)
                     local actualPlayers = 0 --Check so it knows they are actual players.
                     if(#self.eligiblePlayers >= self.playlistClass.minimumPlayers) then --Check so the amount of eligible players has the amount required to have a full match.  
                         --for index = 1, self.playlistClass.maximumPlayers do --Loops from the first index found eligible to the maximumPlayers.
+                        print("More or equal to enough players for a match!")
                           table.foreach(self.eligiblePlayers, function(index, desiredClass)
                             print(index)
                             local desiredClass = self.eligiblePlayers[index] --Gets the current indexed player's class
                             if(desiredClass and desiredClass.playerObject) then 
                                 desiredClass:MatchFound()
-                                table.insert(playersInMatch, self.playerObject)
+                                table.insert(playersInMatch, self.playerObject.Name)
                                 actualPlayers += 1
                             end
                         end)
@@ -55,7 +62,7 @@ function playerQueueClass:SearchForPlayers()
                             MessagingService:PublishAsync("Server Message",data)
                         end
                     end
-                end
+               -- end
             end 
         end 
     end
@@ -71,7 +78,6 @@ function playerQueueClass:HandlePlayerQueue()
     self.playerObject = game.Players:FindFirstChild(self.playerObject)
     self.playerStore = DataStore2("PlayerStore", self.playerObject)
     self.playerSaves = self.playerStore:Get()
-    self.skillRating = self.playerSaves["Skill Rating"]
     coroutine.wrap(function()
         print("About to search for match")
         local foundMatch = self:SearchForPlayers()
