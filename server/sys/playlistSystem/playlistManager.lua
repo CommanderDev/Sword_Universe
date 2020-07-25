6b9d7cfd47f155b6359f8a1e449a89793fd740ca
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-25 01:19:03 -0400

    Update playlistManager.lua

--- server/sys/playlistSystem/playlistManager.lua
+++ server/sys/playlistSystem/playlistManager.lua
@@ -30,6 +30,8 @@ end
 
 local playlistClasses = CreatePlaylistData()
 
+playlistManager.topicFunctions = {}
+
 
 function CreatePlaylistClasses()
     local casual = playlistClasses["Casual"]
@@ -42,10 +44,10 @@ function CreatePlaylistClasses()
     local threeRules: rulesData.rules = {mode = "3v3", minimumPlayers = 6, maximumPlayers = 6, modeType = "Casual"}
     local fourRules: rulesData.rules = {mode = "4v4", minimumPlayers = 8, maximumPlayers = 8, modeType = "Casual"}
 
-    casual["1v1"] = playlistClass.new(oneRules)
-    casual["2v2"] = playlistClass.new(twoRules)
-    casual["3v3"] = playlistClass.new(threeRules)
-    casual["4v4"] = playlistClass.new(fourRules)
+    casual["1v1"] = playlistClass.new(oneRules, playlistManager)
+    casual["2v2"] = playlistClass.new(twoRules,playlistManager)
+    casual["3v3"] = playlistClass.new(threeRules,playlistManager)
+    casual["4v4"] = playlistClass.new(fourRules,playlistManager)
     oneRules.modeType = "Competitive"
     competitive["1v1"] = oneRules
 end 
@@ -59,22 +61,52 @@ function isDefaultData(): boolean
     return true
 end 
 
+
+---[[ Topic Functions]]---
+
+playlistManager.topicFunctions["Set Playlist Data"] = function(playlistData) 
+    print("Setting player data")
+    if(game.JobId == playlistData["Job ID"]) then --Makes sure the server is the one that required the data in the first place.
+         if(not playlistData["Playlist Classes"]) then 
+             print("Creating new classes")
+            CreatePlaylistClasses() 
+         else
+             print("Setting playlist classes to published array")
+            playlistClasses = playlistData["Playlist Classes"]
+        end 
+    end 
+end
+
+playlistManager.topicFunctions["Get Playlist Data"] = function(data)
+    local data = --The data that'll be published to the servers
+    {
+        ["Job ID"] = data.jobId; 
+        ["Playlist Classes"] = nil;
+        Topic = "Set Playlist Data"
+    }
+        if(not isDefaultData) then  --Makes sure the data isn't empty so the server can update to the data all the other servers have
+            data["Playlist Classes"] = playlistClasses
+        end 
+        MessagingService:PublishAsync("Server Message", data)
+     --MessagingService:PublishAsync("Set Playlist Data", data)
+end
+
 function playlistManager:connect() 
     local success, errorMessage = pcall(function()
-    MessagingService:SubscribeAsync("Set Playlist Data", function(playlistData) --Setting the playlist data.
-        local playlistData = playlistData.Data
-        if(game.JobId == playlistData["Job ID"]) then --Makes sure the server is the one that required the data in the first place.
-            if(not playlistData["Playlist Classes"]) then 
-                print("Creating new classes")
-                CreatePlaylistClasses() 
-            else
-                print("Setting playlist classes to published array")
-                playlistClasses = playlistData["Playlist Classes"]
-            end 
-        end 
+    MessagingService:SubscribeAsync("Server Message", function(data)
+        data = data.Data
+        playlistManager.topicFunctions[data.Topic](data)
     end)
 
-    MessagingService:SubscribeAsync("Get Playlist Data", function(jobId) --Get the playlist data
+    local serverMessage =
+    {
+        Topic = "Get Playlist Data";
+        jobId = game.JobId
+    }
+    MessagingService:PublishAsync("Server Message",serverMessage)
+    --MessagingService:PublishAsync("Server Message", publishData)
+end)
+    --[[MessagingService:SubscribeAsync("Get Playlist Data", function(jobId) --Get the playlist data
         local data = --The data that'll be published to the servers
         {
             ["Job ID"] = jobId.Data; 
@@ -86,8 +118,10 @@ function playlistManager:connect()
         end 
         MessagingService:PublishAsync("Set Playlist Data", data)
     end) 
+    
         MessagingService:PublishAsync("Get Playlist Data", game.JobId)
     end)
+    ]]
     if(not success) then 
         print(errorMessage)
         CreatePlaylistClasses()
