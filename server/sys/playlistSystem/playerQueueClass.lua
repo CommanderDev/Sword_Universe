346d718f9270ee3859cf7b436f2f07d4700077ab
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-24 22:15:26 -0400

    Update playerQueueClass.lua

--- server/sys/playlistSystem/playerQueueClass.lua
+++ server/sys/playlistSystem/playerQueueClass.lua
@@ -14,42 +14,55 @@ function playerQueueClass.new(playerObject, playlistClass)
         self.playerSaves = self.playerStore:Get()
         self.skillRating = self.playerSaves["Skill Rating"]
         self.maxSkillGap = 50
-        self.eligiblePlayers = {} --The players eligible to queue
+        self.eligiblePlayers = {self} --The players eligible to queue
+        self.foundMatch = false
     end)()
     return self
 end 
 
 function playerQueueClass:SearchForPlayers()
-    self.eligiblePlayers = {}
+    self.eligiblePlayers = {[1] = self}
+    print("Searching for a match")
     for index, playerClass in next, self.playlistClass.playersInQueue do 
         if(playerClass) then 
             if(self.skillRating-playerClass.skillRating <= self.maxSkillGap or playerClass.skillRating-self.skillRating+self.maxSkillGap <= 0) then 
-                table.insert(self.eligiblePlayers, playerClass)
-                if(#self.eligiblePlayers >= self.playlistClass.minimumPlayers) then 
-                    for index = 1, self.playlistClass.maximumPlayers do --Loops from the first index found eligible to the maximumPlayers.
-                        local desiredClass = self.eligiblePlayers[index] --Gets the current indexed player's class
-                        print(playerClass.playerObject.Name)
-                        if(desiredClass) then 
-                            desiredClass:MatchFound()
+                if(self.playerObject ~= playerClass.playerObject) then 
+                    table.insert(self.eligiblePlayers, playerClass)
+                    if(#self.eligiblePlayers >= self.playlistClass.minimumPlayers) then --Check so the amount of eligible players has the amount required to have a full match.  
+                        for index = 1, self.playlistClass.maximumPlayers do --Loops from the first index found eligible to the maximumPlayers.
+                            print(index)
+                            local desiredClass = self.eligiblePlayers[index] --Gets the current indexed player's class
+                            print(desiredClass.playerObject)
+                            if(desiredClass) then 
+                                desiredClass:MatchFound()
+                            end
                         end
                     end
                 end
-            end
+            end 
         end 
-    end 
+    end
+    return false
 end
 
 function playerQueueClass:MatchFound()
     print(self.playerObject.Name.." Found a match")
+    self.foundMatch = true
     self.playlistClass.playersInQueue[self.playerObject] = nil
     self = nil 
 end
 
 function playerQueueClass:HandlePlayerQueue()
     coroutine.wrap(function()
-        self:SearchForPlayers()
+        local foundMatch = self:SearchForPlayers()
+        if(self.foundMatch) then 
+            return
+        end
         while wait(1) do --Cooldown for each check.
-            self:SearchForPlayers()
+            local foundMatch = self:SearchForPlayers()
+            if(self.foundMatch) then 
+                break
+            end
         end
     end)()
 end
