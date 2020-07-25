febe85a4db7025efaa1ee97b02415dfb79f47c66
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-25 19:10:14 -0400

    Update playlistClass.lua

--- server/sys/playlistSystem/playlistClass.lua
+++ server/sys/playlistSystem/playlistClass.lua
@@ -1,7 +1,8 @@
-local MessagingService = game:GetService("MessagingService")
 
 ---[[ Dependencies ]]---
 local playerQueueClass = _G.get "sys/playlistSystem/playerQueueClass"
+
+local servermessagingManager = _G.get "sys/servermessagingSystem/servermessagingManager"
 
 local DataStore2 = _G.get "DataStore2"
 
@@ -19,6 +20,10 @@ function playlistClass.new(playlistRules, playlistManager)
         end
         self.playersInQueue = {}
         self.playlistManager = playlistManager
+        
+        ---[[ Topics ]]---
+        self.matchBegunTopic = self.modeType.." "..self.mode.." Match Begun"
+        self.addedToQueueTopic = self.modeType.." "..self.mode.." Player Added to queue"
     end)()
     self:HandleMessaging()
     return self
@@ -27,19 +32,32 @@ end
 
 function playlistClass:HandleMessaging()
     local success, errorMessage = pcall(function()
-        --[[MessagingService:SubscribeAsync("Server Message",self.modeType.." "..self.mode.." Match Begun", function(playersInMatch)
-            for index, value in next, playersInMatch do
-                print(value)
-            end
-        end)
-    end)]]
-        self.playlistManager.topicFunctions[self.modeType.." "..self.mode.." Match Begun"] = function(data)
+
+        --[[self.playlistManager.topicFunctions[self.modeType.." "..self.mode.." Match Begun"] = function(data)
             local playersInMatch = data["Players In Match"]
             for index, value in next, playersInMatch do 
                 print(value.." ssuccessfully joined the match!")
             end
         end
-        self.playlistManager.topicFunctions[self.modeType.." "..self.mode.." Player Added to queue"] = function(data)
+        ]]
+        servermessagingManager:SubscribeTopic(self.matchBegunTopic, function(data)
+            local playersInMatch = data["Players In Match"]
+            for index, value in next, playersInMatch do 
+                print(value.." ssuccessfully joined the match!")
+            end
+        end)
+        servermessagingManager:SubscribeTopic(self.addedToQueueTopic, function(data)
+            print(data.playerObject.." added to the queue!")
+            local newPlayerQueue = playerQueueClass.new(data.playerObject, data.skillRating, self)
+            self.playersInQueue[data.playerObject] = newPlayerQueue
+            if(game.Players:FindFirstChild(data.playerObject)) then 
+                data.playerObject = game.Players:FindFirstChild(data.playerObject)
+                newPlayerQueue:HandlePlayerQueue()
+                print("Publishing data received")
+                end
+            end)
+        end)
+       --[[ self.playlistManager.topicFunctions[self.modeType.." "..self.mode.." Player Added to queue"] = function(data)
             print(data.playerObject.." added to the queue!")
             local newPlayerQueue = playerQueueClass.new(data.playerObject, data.skillRating, self)
             self.playersInQueue[data.playerObject] = newPlayerQueue
@@ -50,6 +68,7 @@ function playlistClass:HandleMessaging()
             end
         end
     end)
+    ]]
     if(not success) then 
         print(errorMessage)
     end
@@ -67,7 +86,7 @@ function playlistClass:AddPlayerToQueue(playerObject)
             skillRating = playerSaves["Skill Rating"]
         }
         print("Publishing data!")
-        MessagingService:PublishAsync("Server Message", data)
+        --MessagingService:PublishAsync("Server Message", data)
         print("Data published!")
         --self.playersInQueue[playerObject] = newPlayerQueue 
         --newPlayerQueue:HandlePlayerQueue()
