55a073e80bb7570291d8bbcacd4cbf684360b8e5
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-24 22:46:47 -0400

    Create matchmakingData.lua

---
+++ server/data/matchmakingData.lua
@@ -0,0 +1,6 @@
+local matchmakingData = {}
+
+matchmakingData["minimumSkillGap"] = 10 --The starting skill gap
+matchmakingData["maximumSkillGap"] = 100 --The biggest gap in skill allowed.
+matchmakingData["GapPerLoop"] = 10 --The amount that will be increased everytime the skill gap is to be made higher.
+return matchmakingData 
