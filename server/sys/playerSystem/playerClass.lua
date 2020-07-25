80108d4ebb743f1505b9bfea5956782833454690
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-24 21:25:54 -0400

    Create playerClass.lua

---
+++ server/sys/playerSystem/playerClass.lua
@@ -0,0 +1,35 @@
+---[[ Dependencies ]]---
+local DataStore2 = _G.get "DataStore2"
+
+local playerClass = {}
+playerClass.__index = playerClass 
+
+---[[ Data Store Combine ]]---
+DataStore2.Combine("DATA", "PlayerStore")
+
+function CreatePlayerData() --Creates the default player data 
+    local data = 
+    {
+        ["Skill Rating"] = 0;
+    }
+    return data
+end 
+
+function playerClass.new(playerObject) 
+    local self = setmetatable({}, playerClass)
+    local defaultPlayerData = CreatePlayerData()
+    self.playerObject = playerObject
+    self.playerStore = DataStore2("PlayerStore", playerObject)
+    self.playerSaves = self.playerStore:Get(defaultPlayerData)
+    print(self.playerSaves["Skill Rating"].." is the amount of SR "..playerObject.Name.." has!")
+    self:HandlePlayerEvents()
+    return self
+end 
+
+function playerClass:HandlePlayerEvents()
+    self.playerStore:OnUpdate(function()
+        self.playerSaves = self.playerStore:Get()
+    end)
+end
+
+return playerClass
