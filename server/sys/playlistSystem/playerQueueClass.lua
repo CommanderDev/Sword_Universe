683d85a767ebc8da58d0063739f199b970ab92f1
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-24 22:46:42 -0400

    Update playerQueueClass.lua

--- server/sys/playlistSystem/playerQueueClass.lua
+++ server/sys/playlistSystem/playerQueueClass.lua
@@ -1,5 +1,8 @@
 ---Note: This handles every player that joins the queue and the search for a game as well. This is meant to mainly communicate through playlistClass
 ---[[ Dependencies ]]---
+
+local matchmakingData = _G.get "data/matchmakingData"
+
 local DataStore2 = _G.get "DataStore2"
 
 local playerQueueClass = {}
@@ -13,7 +16,8 @@ function playerQueueClass.new(playerObject, playlistClass)
         self.playerStore = DataStore2("PlayerStore", playerObject)
         self.playerSaves = self.playerStore:Get()
         self.skillRating = self.playerSaves["Skill Rating"]
-        self.maxSkillGap = 50
+        print(self.skillRating.." is the amount of SR "..playerObject.Name.." has!")
+        self.skillgapRadius = matchmakingData["minimumSkillGap"]
         self.eligiblePlayers = {self} --The players eligible to queue
         self.foundMatch = false
     end)()
@@ -22,11 +26,13 @@ end
 
 function playerQueueClass:SearchForPlayers()
     self.eligiblePlayers = {[1] = self}
-    print("Searching for a match")
+    print("Searching for a match The skill gap radius is "..self.skillgapRadius)
     for index, playerClass in next, self.playlistClass.playersInQueue do 
         if(playerClass) then 
-            if(self.skillRating-playerClass.skillRating <= self.maxSkillGap or playerClass.skillRating-self.skillRating+self.maxSkillGap <= 0) then 
-                if(self.playerObject ~= playerClass.playerObject) then 
+            if(self.playerObject ~= playerClass.playerObject) then 
+                print(self.skillRating-playerClass.skillRating)
+                print(0-self.skillRating+playerClass.skillRating)
+                if(self.skillRating-playerClass.skillRating <= self.skillgapRadius and 0-self.skillRating+playerClass.skillRating <= 0-self.skillgapRadius) then 
                     table.insert(self.eligiblePlayers, playerClass)
                     if(#self.eligiblePlayers >= self.playlistClass.minimumPlayers) then --Check so the amount of eligible players has the amount required to have a full match.  
                         for index = 1, self.playlistClass.maximumPlayers do --Loops from the first index found eligible to the maximumPlayers.
@@ -42,11 +48,9 @@ function playerQueueClass:SearchForPlayers()
             end 
         end 
     end
-    return false
 end
 
 function playerQueueClass:MatchFound()
-    print(self.playerObject.Name.." Found a match")
     self.foundMatch = true
     self.playlistClass.playersInQueue[self.playerObject] = nil
     self = nil 
@@ -63,6 +67,13 @@ function playerQueueClass:HandlePlayerQueue()
             if(self.foundMatch) then 
                 break
             end
+        end
+    end)()
+
+    coroutine.wrap(function()
+        while wait(5) do 
+            if(self.foundMatch or self.skillgapRadius + matchmakingData["GapPerLoop"] >= matchmakingData["maximumSkillGap"]) then break end 
+            self.skillgapRadius += matchmakingData["GapPerLoop"]
         end
     end)()
 end
