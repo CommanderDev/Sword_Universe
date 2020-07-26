d0ad5bfd0c64c987440b40a1e0b4cd50a39fa937
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2020-07-26 01:24:38 -0400

    Update playerQueueClass.lua

--- server/sys/playlistSystem/playerQueueClass.lua
+++ server/sys/playlistSystem/playerQueueClass.lua
@@ -1,10 +1,14 @@
 ---Note: This handles every player that joins the queue and the search for a game as well. This is meant to mainly communicate through playlistClass
 
 ---[[ Services ]]---
-local servermessagingManager = _G.get "sys/servermessagingSystem/servermessagingManager"
+local TeleportService = game:GetService("TeleportService")
 
 ---[[ Dependencies ]]---
+local servermessagingManager = _G.get "sys/servermessagingSystem/servermessagingManager"
+
 local matchmakingData = _G.get "data/matchmakingData"
+
+local placeData = _G.get "data/placeData"
 
 local DataStore2 = _G.get "DataStore2"
 
@@ -30,19 +34,23 @@ function playerQueueClass:SearchForPlayers()
     local playersInMatch = {} --Only used if a match is found. Makes it so all the players are added to the match array accordingly.
     for index, playerClass in next, self.playlistClass.playersInQueue do
         if(typeof(playerClass) == "table" and playerClass.playerObject) then 
-            print(playerClass.skillRating)
             if(self.playerObject ~= playerClass.playerObject and playerClass.skillRating) then 
                 if(self.skillgapRadius < self.skillRating - playerClass.skillRating and self.skillgapRadius < self.skillRating+playerClass.skillRating) then return end
                     table.insert(self.eligiblePlayers, playerClass)
                     local actualPlayers = 0 --Check so it knows they are actual players.
                     if(#self.eligiblePlayers >= self.playlistClass.minimumPlayers) then --Check so the amount of eligible players has the amount required to have a full match.  
                         --for index = 1, self.playlistClass.maximumPlayers do --Loops from the first index found eligible to the maximumPlayers.
-                        print("More or equal to enough players for a match!")
                           table.foreach(self.eligiblePlayers, function(index, desiredClass)
-                            print(index)
                             local desiredClass = self.eligiblePlayers[index] --Gets the current indexed player's class
+                            print(desiredCLass)
                             if(desiredClass and desiredClass.playerObject) then 
-                                desiredClass:MatchFound()
+                                local data = 
+                                {
+                                    Topic = "Match Found";
+                                    playerName = self.playerObject.Name
+                                }
+                                servermessagingManager:PublishData(data)
+                                --desiredClass:MatchFound()
                                 table.insert(playersInMatch, self.playerObject.Name)
                                 actualPlayers += 1
                             end
@@ -51,10 +59,10 @@ function playerQueueClass:SearchForPlayers()
                             local data = 
                             {
                                 ["Topic"] = self.playlistClass.modeType.." "..self.playlistClass.mode.." Match Begun";
-                                ["Players In Match"] = playersInMatch
+                                ["Players In Match"] = playersInMatch;
+                                ["Reserve Server"] = TeleportService:ReserveServer(placeData["Match Server"])
                             }
-                            servermessagingManager:PublishData(self.playlistClass.matchBegunTopic, data)
-                           -- MessagingService:PublishAsync("Server Message",data)
+                            servermessagingManager:PublishData(data)
                         end
                     end
                -- end
@@ -75,15 +83,15 @@ function playerQueueClass:HandlePlayerQueue()
     self.playerSaves = self.playerStore:Get()
     coroutine.wrap(function()
         print("About to search for match")
-        local foundMatch = self:SearchForPlayers()
         if(self.foundMatch) then 
             return
         end
+        self:SearchForPlayers()
         while wait(1) do --Cooldown for each check.
-            local foundMatch = self:SearchForPlayers()
             if(self.foundMatch) then 
                 break
             end
+            self:SearchForPlayers()
         end
     end)()
 
